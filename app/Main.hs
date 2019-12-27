dfa96960d791bbc216060f72a5278275ad371b4c
kento tajiri <k.denjiry@gmail.com>
2019-12-27 18:06:39 +0900

    initial work

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,20 @@
 module Main where
 
 import Lib
+import Control.Applicative
+import Data.Char
 
 main :: IO ()
 main = someFunc
 
+newtype Parser a = P (String -> [(a, String)])
 
-type Parser = String -> (Tree, String)
+
+parse :: Parser a -> String -> [(a,String)]
+parse (P p) inp = p inp
+
+item :: Parser Char
+item = P (\inp -> case inp of
+                    [] -> []
+                    (x:xs) -> [(x,xs)])
+
