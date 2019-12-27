5c8defbe4959436999ae3060a0f972ddd2150367
kento tajiri <k.denjiry@gmail.com>
2019-12-27 18:51:45 +0900

    Work parse

--- app/Main.hs
+++ app/Main.hs
@@ -18,3 +18,30 @@ item = P (\inp -> case inp of
                     [] -> []
                     (x:xs) -> [(x,xs)])
 
+
+instance Functor Parser where
+  -- fmap :: (a -> b) -> Parser a -> Parser b
+  fmap g p = P (\inp -> case parse p inp of
+                          [] -> []
+                          [(v,out)] -> [(g v, out)])
+
+instance Applicative Parser where
+  -- pure :: a -> Parser a
+  pure v = P (\inp -> [(v,inp)])
+
+  -- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  pg <*> px = P (\inp -> case parse pg inp of
+                    []->[]
+                    [(g,out)] -> parse (fmap g px) out)
+
+three :: Parser (Char, Char)
+three = do x <- item
+           item
+           z <- item
+           return (x,z)
+
+instance Monad Parser where
+  -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  p >>= f = P (\inp -> case parse p inp of
+                         [] -> []
+                         [(v,out)] -> parse (f v) out)
