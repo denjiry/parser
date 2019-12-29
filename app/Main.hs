7943f64eadd3c6548c456695011c360947b60a40
kento tajiri <k.denjiry@gmail.com>
2019-12-29 19:13:31 +0900

    Work TUI on ghci

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,10 @@ module Main where
 import Lib
 import Control.Applicative
 import Data.Char
+import System.IO
 
 main :: IO ()
-main = someFunc
+main = run
 
 newtype Parser a = P (String -> [(a, String)])
 
@@ -141,8 +142,92 @@ factor = do symbol "("
             return e
           <|> natural -- indent position is very important to avoid runtime-error
 
-eval :: String -> Int
-eval xs = case (parse expr xs) of
-            [(n,[])] -> n
-            [(_,out)] -> error ("Unused input " ++ out)
-            []              -> error "Invalid input"
+-- eval :: String -> Int
+-- eval xs = case (parse expr xs) of
+--             [(n,[])] -> n
+--             [(_,out)] -> error ("Unused input " ++ out)
+--             []              -> error "Invalid input"
+
+box :: [String]
+box = ["+---------------+",
+       "|               |",
+       "+---+---+---+---+",
+       "| q | c | d | = |",
+       "+---+---+---+---+",
+       "| 1 | 2 | 3 | + |",
+       "+---+---+---+---+",
+       "| 4 | 5 | 6 | - |",
+       "+---+---+---+---+",
+       "| 7 | 8 | 9 | * |",
+       "+---+---+---+---+",
+       "| 0 | ( | ) | / |",
+       "+---+---+---+---+"]
+
+buttons :: String
+buttons = standard ++ extra
+  where
+    standard = "qcd=123+456-789*0()/"
+    extra      = "QCD \ESC\BS\DEL\n"
+
+showbox :: IO ()
+showbox = sequence_ [writeat (1,y) b | (y,b) <- zip [1..] box]
+
+getCh :: IO Char
+getCh = do hSetEcho stdin False
+           x <- getChar
+           hSetEcho stdin True
+           return x
+writeat :: Pos -> String -> IO ()
+writeat p xs = do goto p
+                  putStr xs
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+type Pos = (Int,Int)
+
+goto :: Pos -> IO ()
+goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+display :: String -> IO ()
+display xs = do writeat (3,2) (replicate 13 ' ')
+                writeat (3,2) (reverse (take 13 (reverse xs)))
+
+calc :: String -> IO ()
+calc xs = do display xs
+             c <- getCh
+             if elem c buttons then
+                 process c xs
+             else
+                 do beep
+                    calc xs
+
+process :: Char -> String -> IO ()
+process c xs | elem c "qQ\ESC"=quit      
+             | elem c "dD\BS\DEL" =         delete xs 
+             | elem c "=\n"      =           eval xs   
+             | elem c "cC"       = clear                
+             | otherwise = press c xs
+beep :: IO ()
+beep = putStr "\BEL"
+
+quit :: IO ()
+quit = goto (1,14)
+delete :: String -> IO ()
+delete [] = calc []
+delete xs = calc (init xs)
+
+eval :: String -> IO ()
+eval xs = case parse expr xs of
+            [(n,[])] -> calc (show n)
+            _-> do beep
+                   calc xs
+
+clear :: IO ()
+clear = calc []
+press :: Char -> String -> IO ()
+press c xs = calc (xs ++ [c])
+run :: IO ()
+run = do cls
+         showbox
+         clear
